ef7e7faac8cbd83d92b16fe099272916c3c694a0
hohoyoungyoung <xvwjstkvx2@naver.com>
2024-07-12 00:41:45 +0900

    Update 합승 택시 요금.py

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -11,6 +11,8 @@ def solution(n, s, a, b, fares):
         u, v, w = fare
         dp[u][v] = w
         dp[v][u] = w
+
+    # k, i, j 위치 때문에 애먹었다... 순서가 왜 이렇게 되는지 기억.
     
     for k in range(1, n+1):
         for i in range(1, n+1):
