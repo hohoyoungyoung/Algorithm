513fe906db80b0fdde423138e3822e860439d419
hohoyoungyoung <xvwjstkvx2@naver.com>
2024-07-12 00:40:37 +0900

    [level 3] Title: 합승 택시 요금, Time: 154.26 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -0,0 +1,27 @@
+# 최저 택시요금 return
+
+def solution(n, s, a, b, fares):
+    
+    dp = [[float('inf') for _ in range(n+1)] for _ in range(n+1)]
+    
+    for i in range(1, n+1):
+        dp[i][i] = 0
+        
+    for fare in fares:
+        u, v, w = fare
+        dp[u][v] = w
+        dp[v][u] = w
+    
+    for k in range(1, n+1):
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                if dp[i][j] > dp[i][k] + dp[k][j]:
+                    dp[i][j] = dp[i][k] + dp[k][j]
+    
+    answer = float('inf')
+    
+    for t in range(1, n+1):
+        answer = min(answer, dp[s][t] + dp[t][a] + dp[t][b])
+    
+    
+    return answer
