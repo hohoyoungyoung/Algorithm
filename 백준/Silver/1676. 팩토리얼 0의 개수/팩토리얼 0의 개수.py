e3dac8a316c9e6271c2463d9e318d976ca64e74c
hohoyoungyoung <xvwjstkvx2@naver.com>
2025-01-09 14:50:31 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
@@ -1,13 +1,9 @@
-import math
-
-
 def factorial_0(n):
-    
-    i = 1
-    
-    while math.factorial(n) % 10**i == 0:
-        i += 1
-
-    return i-1
+    count = 0
+    i = 5
+    while n >= i:
+        count += n // i  # 5의 배수 개수 더하기
+        i *= 5  # 5의 제곱, 세제곱 등도 고려
+    return count
 
 print(factorial_0(int(input())))
