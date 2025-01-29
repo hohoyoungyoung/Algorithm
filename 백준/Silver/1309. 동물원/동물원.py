a6dbe0e8f128f8e66442f74c82f6363b65e7c847
hohoyoungyoung <xvwjstkvx2@naver.com>
2025-01-29 23:13:37 +0900

    [Silver I] Title: 동물원, Time: 92 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.py
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -1,14 +1,15 @@
-# 메모리 초과
-
 N = int(input())
 
-dp = [0, 0, 0]
-dp[0], dp[1], dp[2] = 1, 1, 1 
+dp = [1, 1, 1]  # 초기 값 설정
 
-for i in range(2, N+1):
+if N == 1:
+    print(sum(dp) % 9901)
+    exit()
+
+for i in range(2, N + 1):
     a, b, c = dp[0], dp[1], dp[2]
-    dp[0] = a+b+c
-    dp[1] = a+c
-    dp[2] = a+b
-    
-print(sum(dp)%9901)
+    dp[0] = (a + b + c) % 9901
+    dp[1] = (a + c) % 9901
+    dp[2] = (a + b) % 9901
+
+print(sum(dp) % 9901)
