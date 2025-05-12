39487f4f9e4fd990d83d1b3f3e323c047e78c28c
hohoyoungyoung <xvwjstkvx2@naver.com>
2025-05-12 21:30:46 +0900

    Time: 24 ms (36.52%), Space: 14 MB (29.29%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,26 +1,19 @@
 class Solution(object):
     def trap(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-
         ans = 0 
         
-        max_left = 0
-        max_right = 0
+        max_left = height[0]
+        max_right = height[-1]
         max_left_list = [0] * len(height)
         max_right_list = [0] * len(height)
 
-        # max_left_list[i]: i 기준 왼쪽에서 가장 큰 높이 (자기 자신 제외)
-        for i in range(1, len(height)):
-            max_left = max(max_left, height[i-1])
+        for i in range(1, len(height)-1):
             max_left_list[i] = max_left
+            max_left = max(max_left, height[i])
 
-        # max_right_list[i]: i 기준 오른쪽에서 가장 큰 높이 (자기 자신 제외)
-        for i in range(len(height)-2, -1, -1):
-            max_right = max(max_right, height[i+1])
+        for i in range(len(height)-2, 0, -1):  
             max_right_list[i] = max_right
+            max_right = max(max_right, height[i])
 
         for i in range(1, len(height)-1):
             min_num = min(max_right_list[i], max_left_list[i])
