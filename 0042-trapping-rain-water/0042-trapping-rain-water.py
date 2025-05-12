79b1a8099dd84808a990413f7506c0da9389f793
hohoyoungyoung <xvwjstkvx2@naver.com>
2025-05-12 16:54:31 +0900

    Time: 35 ms (19.07%), Space: 14.2 MB (16.68%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,24 +1,30 @@
 class Solution(object):
     def trap(self, height):
-        if not height:
-            return 0
+        """
+        :type height: List[int]
+        :rtype: int
+        """
 
-        n = len(height)
-        max_left_list = [0] * n
-        max_right_list = [0] * n
+        ans = 0 
+        
+        max_left = 0
+        max_right = 0
+        max_left_list = [0] * len(height)
+        max_right_list = [0] * len(height)
 
-        max_left_list[0] = height[0]
-        for i in range(1, n):
-            max_left_list[i] = max(max_left_list[i-1], height[i])
+        # max_left_list[i]: i 기준 왼쪽에서 가장 큰 높이 (자기 자신 제외)
+        for i in range(1, len(height)):
+            max_left = max(max_left, height[i-1])
+            max_left_list[i] = max_left
 
-        max_right_list[n-1] = height[n-1]
-        for i in range(n-2, -1, -1):
-            max_right_list[i] = max(max_right_list[i+1], height[i])
+        # max_right_list[i]: i 기준 오른쪽에서 가장 큰 높이 (자기 자신 제외)
+        for i in range(len(height)-2, -1, -1):
+            max_right = max(max_right, height[i+1])
+            max_right_list[i] = max_right
 
-        ans = 0
-        for i in range(n):
-            water = min(max_left_list[i], max_right_list[i]) - height[i]
-            if water > 0:
-                ans += water
+        for i in range(1, len(height)-1):
+            min_num = min(max_right_list[i], max_left_list[i])
+            if min_num > height[i]:
+                ans += min_num - height[i]
 
         return ans
