0e4577360e6fb7fc89c24aa567e262ca0cde21db
hohoyoungyoung <xvwjstkvx2@naver.com>
2025-06-25 16:55:06 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 236 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,30 @@
+#import sys
+#input = sys.stdin.readline
+
+
+while True:
+    line = input()
+    if line == '.':
+        break
+
+    stack = []
+    balanced = True
+
+    for char in line:
+        if char == '(' or char == '[':
+            stack.append(char)
+        elif char == ')':
+            if not stack or stack[-1] != '(':
+                balanced = False
+                break
+            stack.pop()
+        elif char == ']':
+            if not stack or stack[-1] != '[':
+                balanced = False
+                break
+            stack.pop()
+
+    if balanced and not stack:
+        print('yes')
+    else:
+        print('no')
