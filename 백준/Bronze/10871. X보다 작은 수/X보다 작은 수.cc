d7e76cf63698e10a443ac3724cd6333ce4e7ea8e
hohoyoungyoung <xvwjstkvx2@naver.com>
2025-06-30 16:31:54 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -2,10 +2,13 @@
 using namespace std;
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
     int N, X;
     cin >> N >> X;
 
-    for (int i=0; i<N; ++i) {
+    for (int i = 0; i < N; ++i) {
         int num;
         cin >> num;
         if (num < X) {
