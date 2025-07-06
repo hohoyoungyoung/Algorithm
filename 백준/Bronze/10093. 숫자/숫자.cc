98e44324951ce6294e57ed5be2c434255943d85d
hohoyoungyoung <xvwjstkvx2@naver.com>
2025-07-06 21:12:37 +0900

    [Bronze II] Title: 숫자, Time: 16 ms, Memory: 2804 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.cc
+++ 백준/Bronze/10093. 숫자/숫자.cc
@@ -4,28 +4,25 @@
 using namespace std;
 
 int main() {
-    int A, B;
+    long long A, B;
     cin >> A >> B;
 
-    int low = min(A, B);
-    int high = max(A, B);
-    int C = high - low;
+    long long low = min(A, B);
+    long long high = max(A, B);
+    long long C = high - low;
 
     if (C <= 1) {
-        cout << 0 << '\n';  // 사이에 수 없음
+        cout << 0 << '\n';
         return 0;
     }
 
-    vector<int> vec(C - 1);
+    vector<long long> vec(C - 1);
 
     cout << C - 1 << '\n';
 
-    for (int i = low + 1; i < high; i++) {
+    for (long long i = low + 1; i < high; i++) {
         vec[i - low - 1] = i;
     }
-
-    // 정렬은 이미 오름차순이지만 요구되면 유지
-    sort(vec.begin(), vec.end());
 
     for (auto i : vec) {
         cout << i << ' ';
