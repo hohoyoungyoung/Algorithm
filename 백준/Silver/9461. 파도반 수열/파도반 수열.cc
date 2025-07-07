257225e62f5935a47bd7659a45b3981fbe8e0578
hohoyoungyoung <xvwjstkvx2@naver.com>
2025-07-07 16:38:22 +0900

    [Silver III] Title: 파도반 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.cc
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -1,23 +1,29 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-long long dp[101];  // N은 최대 100
+vector<long long> precompute(int max_n) {
+    vector<long long> dp(max_n + 1, 0);  // 1-based indexing을 위해 +1
 
-void precompute() {
     dp[1] = dp[2] = dp[3] = 1;
     dp[4] = dp[5] = 2;
-    for (int i = 6; i <= 100; i++) {
+
+    for (int i = 6; i <= max_n; i++) {
         dp[i] = dp[i - 1] + dp[i - 5];
     }
+
+    return dp;
 }
 
 int main() {
-    int T, N;
+    int T;
     cin >> T;
 
-    precompute();  // 파도반 수열 미리 계산
+    // 문제 조건에서 N ≤ 100이므로 미리 계산
+    vector<long long> dp = precompute(100);
 
     while (T--) {
+        int N;
         cin >> N;
         cout << dp[N] << '\n';
     }
