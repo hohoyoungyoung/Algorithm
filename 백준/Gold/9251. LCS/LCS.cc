22b79cf40044565b26de270e5cbb3e8c03b9b7bb
hohoyoungyoung <xvwjstkvx2@naver.com>
2025-07-13 18:43:17 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5984 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm> // max
+
+using namespace std;
+
+int main() {
+    string A, B;
+    cin >> A >> B;
+
+    int n = A.size();
+    int m = B.size();
+
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            if (A[i - 1] == B[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+            else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    cout << dp[n][m] << endl;
+
+    return 0;
+}
