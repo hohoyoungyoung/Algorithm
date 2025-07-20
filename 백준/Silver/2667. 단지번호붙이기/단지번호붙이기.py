585622106dd40961651b91c312986cd185e13310
hohoyoungyoung <xvwjstkvx2@naver.com>
2025-07-20 14:28:10 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 32488 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -3,37 +3,31 @@ N = int(input())
 maps = []
 result = []
 
-# 지도 입력
-for _ in range(N):
+for i in range(N):
     maps.append(input())
 
-# 방문 여부 기록
-visited = [[0] * N for _ in range(N)]
+visited = [[0]*N for i in range(N)]
+direction = [(-1,0),(0,-1),(0,1),(1,0)]
 
-# 상하좌우 방향 설정
-direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-# DFS 정의
 def dfs(x, y):
-    global count
-    for dx, dy in direction:
-        nx, ny = x + dx, y + dy
-        if 0 <= nx < N and 0 <= ny < N:
-            if maps[nx][ny] == '1' and visited[nx][ny] == 0:
-                visited[nx][ny] = 1
-                count += 1
-                dfs(nx, ny)
+    for dx,dy in direction:
+        if 0 <= x+dx < N and 0 <= y+dy < N and maps[x+dx][y+dy] == '1' and visited[x+dx][y+dy] == 0:
+                 
+            visited[x+dx][y+dy] = 1
+            dfs(x+dx, y+dy)
+            global count
+            count += 1
 
-# 전체 지도 순회
+
 for i in range(N):
     for j in range(N):
-        if maps[i][j] == '1' and visited[i][j] == 0:
+        if maps[i][j] == '1' and not visited[i][j]:
+            visited[i][j] = 1
             count = 1
-            visited[i][j] = 1  # 시작점 방문 체크!
             dfs(i, j)
             result.append(count)
 
-# 출력
-print(len(result))         # 총 단지 수
-for val in sorted(result): # 단지 내 집 수 오름차순 출력
-    print(val)
+print(len(result))
+result.sort()
+for i in range(len(result)):
+    print(result[i])
