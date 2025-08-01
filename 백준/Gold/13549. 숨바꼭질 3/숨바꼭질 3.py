74ee6c19bd1fe6f881006b79dbbae35a4acd2f5e
hohoyoungyoung <xvwjstkvx2@naver.com>
2025-08-01 11:23:48 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 168 ms, Memory: 38148 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,22 +1,23 @@
-import heapq
+from collections import deque
 
-N, K = map(int, input().split(' '))
-INF = float('inf')
-distance = [INF] * 100001
+N, K = map(int, input().split())
+INF = int(1e9)
+distance = [-1] * 100001
+distance[N] = 0
 
-if N >= K:
-    print(N-K)
-else:
-    heap = [(0, N)]
-    distance[N] = 0
+dq = deque()
+dq.append(N)
 
-    while heap:
-        time, pos = heapq.heappop(heap)
+while dq:
+    pos = dq.popleft()
 
-        for next_pos, cost in [(pos*2, 0), (pos-1, 1), (pos+1, 1)]:
-            if (0<=next_pos<=100000):
-                if (distance[next_pos] > time + cost):
-                    distance[next_pos] = time + cost
-                    heapq.heappush(heap, (distance[next_pos], next_pos))
+    for next_pos, cost in [(pos * 2, 0), (pos - 1, 1), (pos + 1, 1)]:
+        if 0 <= next_pos <= 100000:
+            if distance[next_pos] == -1 or distance[next_pos] > distance[pos] + cost:
+                distance[next_pos] = distance[pos] + cost
+                if cost == 0:
+                    dq.appendleft(next_pos)
+                else:
+                    dq.append(next_pos)
 
-    print(distance[K])
+print(distance[K])
