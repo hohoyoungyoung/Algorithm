270cf0e82fdeed56e511316089fe494812a9e911
hohoyoungyoung <xvwjstkvx2@naver.com>
2025-08-28 16:29:16 +0900

    [level 2] Title: 최솟값 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59038. 최솟값 구하기/최솟값 구하기.sql
+++ 프로그래머스/2/59038. 최솟값 구하기/최솟값 구하기.sql
@@ -1,5 +1,10 @@
 -- 코드를 입력하세요
-SELECT DATETIME as '시간'
-from ANIMAL_INS a
-WHERE a.DATETIME = (SELECT MIN(DATETIME)
-                   FROM ANIMAL_INS b)
+#SELECT DATETIME as '시간'
+#from ANIMAL_INS a
+#WHERE a.DATETIME = (SELECT MIN(DATETIME)
+#                   FROM ANIMAL_INS b)
+                   
+                   
+
+SELECT MIN(DATETIME)
+FROM ANIMAL_INS
