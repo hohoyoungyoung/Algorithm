bdf8cd14cb0e040b5247f6603e2f1ac6d54454e4
hohoyoungyoung <xvwjstkvx2@naver.com>
2025-09-09 14:08:32 +0900

    [Silver III] Title: 소수 구하기, Time: 12 ms, Memory: 5860 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -1,24 +1,26 @@
-#include <vector>
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main() {
     int M, N;
     cin >> M >> N;
 
-    vector<int> chae(N+1, 0);
-    
-    
-    for (int i=2; i*i<=N; i++) {
-        if (chae[i] == 1) continue;
-        for (int j=2; i*j < N+1; j++) {
-            chae[i*j] = 1;
+    vector<int> chae(N+1, 0);  // 0=소수 후보, 1=합성수
+
+    chae[0] = chae[1] = 1; // 0,1은 소수 아님
+
+    for (int i = 2; i * i <= N; i++) {
+        if (chae[i] == 0) { // 아직 소수라면
+            for (int j = 2 * i; j <= N; j += i) {
+                chae[j] = 1; // 배수는 합성수로 마킹
+            }
         }
     }
 
-    for (int i=M; i<N+1; i++) {
-        if (i==1) continue;
+    for (int i = M; i <= N; i++) {
         if (chae[i] == 0) cout << i << '\n';
     }
+
+    return 0;
 }
