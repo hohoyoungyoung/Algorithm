96f98301fc24bb4f38f8b1946f460f6f2208d2e0
hohoyoungyoung <xvwjstkvx2@naver.com>
2025-09-13 23:27:52 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 3.85 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
@@ -1,40 +1,29 @@
-// 무조건 다음수는 커져야함
-// 
 #include <string>
 #include <vector>
-
 using namespace std;
 
 string binary(int n) {
-    if (n==1) return "1";
+    if (n == 1) return "1";
     string bin_string = "";
-    while (n>0) {
-        bin_string = bin_string + to_string(n % 2);
+    while (n > 0) {
+        // 앞에 붙여서 올바른 순서로 저장
+        bin_string = to_string(n % 2) + bin_string;
         n = n / 2;
     }
-    
     return bin_string;
 }
 
 int one_check(string num) {
     int one_count = 0;
-    
-    for (char i: num) {
-    if (i=='1') one_count++;    
+    for (char i : num) {
+        if (i == '1') one_count++;
     }
-    
     return one_count;
 }
 
-
 int solution(int n) {
-    int i = n;
     int target = one_check(binary(n));
-    
-    while (true) {
-        i++;
-        if (one_check(binary(i)) == target) break;        
+    for (int i = n + 1;; i++) {
+        if (one_check(binary(i)) == target) return i;
     }
-    
-    return i;
 }
